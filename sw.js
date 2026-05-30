aeeca3d4d424f371574666e71f892647993394fd
bradcmgmt <brad@r-m.art>
2026-05-30 11:29:15 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * Bump CACHE_VERSION whenever the shell changes so old caches get purged.
  */
 
-const CACHE_VERSION = 'bandmate-v6-app';
+const CACHE_VERSION = 'bandmate-v7-app';
 const APP_SHELL = [
   '/',
   '/index.html',
